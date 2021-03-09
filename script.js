d73e465a143407e2a22f73f78f307de500f67286
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-03-09 09:58:06 -0500

    modified style and script

--- script.js
+++ script.js
@@ -23,44 +23,44 @@ function changeQuestionFunction() {
 //creating question objects
 let qAndA1 = {
   question: "Commonly used data types DO NOT include:",
-  answer1: "strings",
-  answer2: "booleans",
-  answer3: "alerts",
-  answer4: "numbers",
+  answer1: "1. strings",
+  answer2: "2. booleans",
+  answer3: "3. alerts",
+  answer4: "4. numbers",
   correctAnswer: "alerts",
 };
 let qAndA2 = {
-  question: "The condition in an if / else statement is enclose withing ____.",
-  answer1: "quotes",
-  answer2: "curly brackets",
-  answer3: "parenthesis",
-  answer4: "square brackets",
+  question: "The condition in an if / else statement is enclose within ____.",
+  answer1: "1. quotes",
+  answer2: "2. curly brackets",
+  answer3: "3. parenthesis",
+  answer4: "4. square brackets",
   correctAnswer: "parenthesis",
 };
 let qAndA3 = {
   question: "Arrays in JavaScript can be used to store ____.",
-  answer1: "numbers and strings",
-  answer2: "other arrays",
-  answer3: "booleans",
-  answer4: "all of the above",
+  answer1: "1. numbers and strings",
+  answer2: "2. other arrays",
+  answer3: "3. booleans",
+  answer4: "4. all of the above",
   correctAnswer: "all of the above",
 };
 let qAndA4 = {
   question:
     "String values must be enclosed within ___ when being assigned to variables",
-  answer1: "commas",
-  answer2: "curly brackets",
-  answer3: "quotes",
-  answer4: "parenthesis",
+  answer1: "1. commas",
+  answer2: "2. curly brackets",
+  answer3: "3. quotes",
+  answer4: "4. parenthesis",
   correctAnswer: "quotes",
 };
 let qAndA5 = {
   question:
     "A very useful tool used during development and debugging for printing content to the debugger is",
-  answer1: "javascript",
-  answer2: "terminal / bash",
-  answer3: "for loops",
-  answer4: "console.log",
+  answer1: "1. javascript",
+  answer2: "2. terminal / bash",
+  answer3: "3. for loops",
+  answer4: "4. console.log",
   correctAnswer: "console.log",
 };
 
@@ -79,71 +79,88 @@ button.addEventListener("click", () => {
     answerButton.style.display = "block";
   }
 });
-
+//buttons
+//button one
 answerButton1.addEventListener("click", () => {
-  if (answerButton1.innerText === questionsList[listNumber].correctAnswer) {
+  if (
+    answerButton1.innerText.slice(3) === questionsList[listNumber].correctAnswer
+  ) {
     correctIncorrect.innerText = "You Are Correct!";
+    correctIncorrect.style.color = "green";
     totalScore += 1;
     changeQuestionFunction();
     setTimeout(() => {
       correctIncorrect.innerText = "";
-    }, 2000);
+    }, 1000);
   } else {
+    correctIncorrect.style.color = "red";
     correctIncorrect.innerText = "Incorrect";
     changeQuestionFunction();
     setTimeout(() => {
       correctIncorrect.innerText = "";
-    }, 2000);
+    }, 1000);
   }
 });
-
+//button 2
 answerButton2.addEventListener("click", () => {
-  if (answerButton2.innerText === questionsList[listNumber].correctAnswer) {
+  if (
+    answerButton2.innerText.slice(3) === questionsList[listNumber].correctAnswer
+  ) {
+    correctIncorrect.style.color = "green";
     correctIncorrect.innerText = "You Are Correct!";
     totalScore += 1;
     changeQuestionFunction();
     setTimeout(() => {
       correctIncorrect.innerText = "";
-    }, 2000);
+    }, 1000);
   } else {
+    correctIncorrect.style.color = "red";
     correctIncorrect.innerText = "Incorrect";
     changeQuestionFunction();
     setTimeout(() => {
       correctIncorrect.innerText = "";
-    }, 2000);
+    }, 1000);
   }
 });
-
+//button 3
 answerButton3.addEventListener("click", () => {
-  if (answerButton3.innerText === questionsList[listNumber].correctAnswer) {
+  if (
+    answerButton3.innerText.slice(3) === questionsList[listNumber].correctAnswer
+  ) {
+    correctIncorrect.style.color = "green";
     correctIncorrect.innerText = "You Are Correct!";
     totalScore += 1;
     changeQuestionFunction();
     setTimeout(() => {
       correctIncorrect.innerText = "";
-    }, 2000);
+    }, 1000);
   } else {
+    correctIncorrect.style.color = "red";
     correctIncorrect.innerText = "Incorrect";
     changeQuestionFunction();
     setTimeout(() => {
       correctIncorrect.innerText = "";
-    }, 2000);
+    }, 1000);
   }
 });
-
+//button 4
 answerButton4.addEventListener("click", () => {
-  if (answerButton4.innerText === questionsList[listNumber].correctAnswer) {
+  if (
+    answerButton4.innerText.slice(3) === questionsList[listNumber].correctAnswer
+  ) {
+    correctIncorrect.style.color = "green";
     correctIncorrect.innerText = "You Are Correct!";
     totalScore += 1;
     changeQuestionFunction();
     setTimeout(() => {
       correctIncorrect.innerText = "";
-    }, 2000);
+    }, 1000);
   } else {
+    correctIncorrect.style.color = "red";
     correctIncorrect.innerText = "Incorrect";
     changeQuestionFunction();
     setTimeout(() => {
       correctIncorrect.innerText = "";
-    }, 2000);
+    }, 1000);
   }
 });

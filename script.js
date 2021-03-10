7a72edf30fd33ac2a160dd54d848aa910013f8c2
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-03-09 19:57:12 -0500

    added a whole lot more javascript probably more than i should have before a commit

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 let button = document.querySelector("#start-button");
 let h1 = document.querySelector("h1");
 let p = document.querySelector("p");
+let main = document.querySelector("main");
+let aside = document.createElement("aside");
+let section = document.querySelector("section");
+let form = document.querySelector("form");
+let input = document.querySelector("input");
+//timer append
+main.append(aside);
+let timer = document.querySelector("#timer");
+
 //all answer buttons
 let answerButtons = document.querySelectorAll(".answer-buttons");
 //individual answer buttons
@@ -57,127 +66,171 @@ let qAndA5 = {
 
 let questionsList = [qAndA1, qAndA2, qAndA3, qAndA4, qAndA5];
 
+let timeLeft = 60;
+let listNumber = -1;
+form.style.display = "none";
 //initial screen button, hides main button and uses the change question function to fill those buttons and h1 with the correct information.  starts timer.
 button.addEventListener("click", () => {
   //change question function which changes questions as they are chosen.
   changeQuestionFunction();
+  //hide paragraph and form
   p.style.display = "none";
+
   //hide the main button
   button.style.display = "none";
-
   //display the answer buttons
   for (answerButton of answerButtons) {
     answerButton.style.display = "block";
   }
-});
+  //changes the question and answer choices until setting off allDone function to clear the screen for final score.
 
-//changes the question and answer choices until setting off allDone function to clear the screen for final score.
-let listNumber = -1;
+  //start interval
+  let stopInterval = setInterval(() => {
+    aside.innerText = "Time " + timeLeft;
+    timeLeft -= 1;
+    //if time runs out run allDone() clear the interval
+    if (timeLeft === 0) {
+      allDone();
+      clearInterval(stopInterval);
+    }
+  }, 1000);
 
-function changeQuestionFunction() {
-  if (listNumber <= 3) {
-    h1.innerText = questionsList[(listNumber += 1)].question;
-    answerButton1.innerText = questionsList[listNumber].answer1;
-    answerButton2.innerText = questionsList[listNumber].answer2;
-    answerButton3.innerText = questionsList[listNumber].answer3;
-    answerButton4.innerText = questionsList[listNumber].answer4;
-  } else {
-    allDone();
+  function changeQuestionFunction() {
+    if (listNumber <= 3) {
+      h1.innerText = questionsList[(listNumber += 1)].question;
+      answerButton1.innerText = questionsList[listNumber].answer1;
+      answerButton2.innerText = questionsList[listNumber].answer2;
+      answerButton3.innerText = questionsList[listNumber].answer3;
+      answerButton4.innerText = questionsList[listNumber].answer4;
+    } else {
+      allDone();
+    }
   }
-}
 
-let totalScore = 0;
-//answer buttons
-//button one
-answerButton1.addEventListener("click", () => {
-  if (
-    answerButton1.innerText.slice(3) === questionsList[listNumber].correctAnswer
-  ) {
-    correctIncorrect.innerText = "You Are Correct!";
-    correctIncorrect.style.color = "green";
-    totalScore += 1;
-    changeQuestionFunction();
-    setTimeout(() => {
-      correctIncorrect.innerText = "";
-    }, 1000);
-  } else {
-    correctIncorrect.style.color = "red";
-    correctIncorrect.innerText = "Incorrect";
-    changeQuestionFunction();
-    setTimeout(() => {
-      correctIncorrect.innerText = "";
-    }, 1000);
-  }
-});
-//button 2
-answerButton2.addEventListener("click", () => {
-  if (
-    answerButton2.innerText.slice(3) === questionsList[listNumber].correctAnswer
-  ) {
-    correctIncorrect.style.color = "green";
-    correctIncorrect.innerText = "You Are Correct!";
-    totalScore += 1;
-    changeQuestionFunction();
-    setTimeout(() => {
-      correctIncorrect.innerText = "";
-    }, 1000);
-  } else {
-    correctIncorrect.style.color = "red";
-    correctIncorrect.innerText = "Incorrect";
-    changeQuestionFunction();
-    setTimeout(() => {
-      correctIncorrect.innerText = "";
-    }, 1000);
-  }
-});
-//button 3
-answerButton3.addEventListener("click", () => {
-  if (
-    answerButton3.innerText.slice(3) === questionsList[listNumber].correctAnswer
-  ) {
-    correctIncorrect.style.color = "green";
-    correctIncorrect.innerText = "You Are Correct!";
-    totalScore += 1;
-    changeQuestionFunction();
-    setTimeout(() => {
-      correctIncorrect.innerText = "";
-    }, 1000);
-  } else {
-    correctIncorrect.style.color = "red";
-    correctIncorrect.innerText = "Incorrect";
-    changeQuestionFunction();
-    setTimeout(() => {
-      correctIncorrect.innerText = "";
-    }, 1000);
-  }
-});
-//button 4
-answerButton4.addEventListener("click", () => {
-  if (
-    answerButton4.innerText.slice(3) === questionsList[listNumber].correctAnswer
-  ) {
-    correctIncorrect.style.color = "green";
-    correctIncorrect.innerText = "You Are Correct!";
-    totalScore += 1;
-    changeQuestionFunction();
-    setTimeout(() => {
-      correctIncorrect.innerText = "";
-    }, 1000);
-  } else {
-    correctIncorrect.style.color = "red";
-    correctIncorrect.innerText = "Incorrect";
-    changeQuestionFunction();
-    setTimeout(() => {
-      correctIncorrect.innerText = "";
-    }, 1000);
-  }
-});
-//clears questions and buttons and displays final score
-function allDone() {
-  for (let answerButton of answerButtons) {
-    answerButton.style.display = "none";
+  //answer buttons
+  //button one
+  answerButton1.addEventListener("click", () => {
+    if (
+      answerButton1.innerText.slice(3) ===
+      questionsList[listNumber].correctAnswer
+    ) {
+      correctIncorrect.innerText = "You Are Correct!";
+      correctIncorrect.style.color = "green";
 
-    correctIncorrect.style.display = "none";
+      changeQuestionFunction();
+      setTimeout(() => {
+        correctIncorrect.innerText = "";
+      }, 1000);
+    } else {
+      timeLeft -= 10;
+      correctIncorrect.style.color = "red";
+      correctIncorrect.innerText = "Incorrect";
+      changeQuestionFunction();
+
+      setTimeout(() => {
+        correctIncorrect.innerText = "";
+      }, 1000);
+    }
+  });
+  //button 2
+  answerButton2.addEventListener("click", () => {
+    if (
+      answerButton2.innerText.slice(3) ===
+      questionsList[listNumber].correctAnswer
+    ) {
+      correctIncorrect.style.color = "green";
+      correctIncorrect.innerText = "You Are Correct!";
+
+      changeQuestionFunction();
+      setTimeout(() => {
+        correctIncorrect.innerText = "";
+      }, 1000);
+    } else {
+      timeLeft -= 10;
+      correctIncorrect.style.color = "red";
+      correctIncorrect.innerText = "Incorrect";
+      changeQuestionFunction();
+
+      setTimeout(() => {
+        correctIncorrect.innerText = "";
+      }, 1000);
+    }
+  });
+  //button 3
+  answerButton3.addEventListener("click", () => {
+    if (
+      answerButton3.innerText.slice(3) ===
+      questionsList[listNumber].correctAnswer
+    ) {
+      correctIncorrect.style.color = "green";
+      correctIncorrect.innerText = "You Are Correct!";
+
+      changeQuestionFunction();
+      setTimeout(() => {
+        correctIncorrect.innerText = "";
+      }, 1000);
+    } else {
+      timeLeft -= 10;
+      correctIncorrect.style.color = "red";
+      correctIncorrect.innerText = "Incorrect";
+      changeQuestionFunction();
+
+      setTimeout(() => {
+        correctIncorrect.innerText = "";
+      }, 1000);
+    }
+  });
+  //button 4
+  answerButton4.addEventListener("click", () => {
+    if (
+      answerButton4.innerText.slice(3) ===
+      questionsList[listNumber].correctAnswer
+    ) {
+      correctIncorrect.style.color = "green";
+      correctIncorrect.innerText = "You Are Correct!";
+
+      changeQuestionFunction();
+      setTimeout(() => {
+        correctIncorrect.innerText = "";
+      }, 1000);
+    } else {
+      timeLeft -= 10;
+      correctIncorrect.style.color = "red";
+      correctIncorrect.innerText = "Incorrect";
+      changeQuestionFunction();
+
+      setTimeout(() => {
+        correctIncorrect.innerText = "";
+      }, 1000);
+    }
+  });
+
+  //clears questions and buttons and displays final score
+  function allDone() {
+    for (let answerButton of answerButtons) {
+      answerButton.style.display = "none";
+      correctIncorrect.style.display = "none";
+    }
+    //h1 All Done!
     h1.innerText = "All Done!";
+    //p
+    p.innerText = "Your Final Score is " + timeLeft + " points!";
+    p.style.display = "block";
+    //form display
+    form.style.display = "block";
+    //timer changes to score
+    aside.innerText = "Score " + timeLeft + "!";
+    //stopping timer
+    clearInterval(stopInterval);
   }
-}
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+  input.addEventListener("change", (e) => {
+    console.log(e.target.value);
+
+    h1.innerText = "High Scores";
+    p.style.display = "none";
+    form.style.display = "none";
+  });
+});
